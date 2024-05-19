ca31dfe1c28aa2a200d583bf703fef952c29b5f2
BenjaminS <97973081+benjisho@users.noreply.github.com>
2024-05-19 14:16:29 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,28 @@
-print("hello bro whats up")
+import os
 
+def install_packages():
+    # Update package database and upgrade system
+    os.system('pacman -Syu --noconfirm')
+
+    # Install official packages
+    official_packages = [
+        'libnma',  # Example package
+        # Add other official packages here
+    ]
+    os.system(f'pacman -Sy --noconfirm {" ".join(official_packages)}')
+
+    # Install AUR helper (yay)
+    os.system('pacman -Sy --noconfirm base-devel git')
+    os.system('git clone https://aur.archlinux.org/yay.git /tmp/yay')
+    os.chdir('/tmp/yay')
+    os.system('makepkg -si --noconfirm')
+
+    # Install AUR packages
+    aur_packages = [
+        'google-chrome',  # Example package
+        # Add other AUR packages here
+    ]
+    os.system(f'yay -S --noconfirm {" ".join(aur_packages)}')
+
+if __name__ == "__main__":
+    install_packages()
